1ba83f2ae522d05fab2279300149d447231786c9
AO Industries, Inc <support@aoindustries.com>
2020-02-26 23:55:06 -0600

    Optimized ServletContext attribute initialization and concurrency.

--- src/main/java/com/semanticcms/section/style/Initializer.java
+++ src/main/java/com/semanticcms/section/style/Initializer.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-section-style - Default style for sections nested within SemanticCMS pages and elements.
- * Copyright (C) 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -29,7 +29,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener("Registers the styles for sections in HtmlRenderer.")
-public class SectionStyleContextListener implements ServletContextListener {
+public class Initializer implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
