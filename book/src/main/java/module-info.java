e0961f38624409a371b3dc40097f99f19b066b86
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:45:22 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-section-style.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-section-style.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.section.style.book {
 	exports com.semanticcms.section.style.book;
